3acf846d967b08502cd841eead83bccae9c0641b
ADMIN <aribooluwatoba.aribo@gmail.com>
2021-03-07 02:19:03 +0100

    updated project files

---
+++ src/test/java/com/github/elmanuel1/webclientlibray/WebclientLibrayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.elmanuel1.webclientlibray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebclientLibrayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
